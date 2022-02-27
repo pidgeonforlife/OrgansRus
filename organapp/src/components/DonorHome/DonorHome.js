3fb46616f75283ecdc9638b09fbc1a729ad45fd2
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-02-26 22:42:17 -0800

    Implemented the Recepient list

--- organapp/src/components/DonorHome/DonorHome.js
+++ organapp/src/components/DonorHome/DonorHome.js
@@ -34,26 +34,27 @@ export class DonorHome extends Component {
                   </div>
                   <div className='recipientDiv'>
                       <div className='recipientImg'>
-                        <img src='./images/redhead.png' />
+                        <img src='./images/blacklady.png' />
                       </div>
                       <div className='recipientDescription'>
-                          <a className='fix'><img id='smaller' src='./images/usericon.png' />Sarah Melanie</a>
-                          <h4>Need Lung Transplant!</h4>
-                          <p>Hi my name is Sarah, and I enjoy going out for walks on the beach. Unfortunately
-                              from a sudden illness, my left lung ..
+                          <a className='fix'><img id='smaller' src='./images/usericon.png' />Dae Hwan</a>
+                          <h4>Urgent Need of Kidney!</h4>
+                          <p>
+                              Dae is a teacher and mother of four children, recently she went through a car accident
+                              leaving her ..
                           </p>
                           <a id='bringdown' href=''>Read More <img id='down' src='./images/arrowright.png' /></a>
                       </div>
                   </div>
                   <div className='recipientDiv'>
                       <div className='recipientImg'>
-                        <img src='./images/redhead.png' />
+                        <img src='./images/asianman.png' />
                       </div>
                       <div className='recipientDescription'>
-                          <a className='fix'><img id='smaller' src='./images/usericon.png' />Sarah Melanie</a>
-                          <h4>Need Lung Transplant!</h4>
-                          <p>Hi my name is Sarah, and I enjoy going out for walks on the beach. Unfortunately
-                              from a sudden illness, my left lung ..
+                          <a className='fix'><img id='smaller' src='./images/usericon.png' />Eric Choi</a>
+                          <h4>Spare A Liver?</h4>
+                          <p>Hello, my name is Eric. Due to constant stress of hackathons, I've drank a lot, and my doctors
+                              require me to ..
                           </p>
                           <a id='bringdown' href=''>Read More <img id='down' src='./images/arrowright.png' /></a>
                       </div>
